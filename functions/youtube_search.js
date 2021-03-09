eea92cfd64947a5824b385b8572d5c0b5824e950
seojihwan <seojihwan213@naver.com>
2021-03-09 20:58:23 +0900

    feat: youtube search api

---
+++ functions/youtube_search.js
@@ -0,0 +1,46 @@
+const fetch = require('node-fetch');
+
+const YOUTUBE_SEARCH = 'https://www.googleapis.com/youtube/v3/search';
+
+const getQueryString = params => {
+  let queryString = '?';
+  for (const [key, value] of Object.entries(params)) {
+    queryString += `&${key}=${value}`;
+  }
+
+  return queryString;
+};
+
+const headers = {
+  'Access-Control-Allow-Origin':
+    'https://seojihwan.github.io/javascript-youtube-classroom',
+};
+
+exports.handler = async event => {
+  const { queryStringParameters } = event;
+  try {
+    const response = await fetch(
+      YOUTUBE_SEARCH +
+        getQueryString({
+          ...queryStringParameters(event),
+          key: process.env.API_KEY,
+        })
+    );
+
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({
+        data: response,
+      }),
+    };
+  } catch (err) {
+    return {
+      statusCode: err.statusCode,
+      headers,
+      body: JSON.stringify({
+        error: err.message,
+      }),
+    };
+  }
+};
